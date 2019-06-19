d8a6a879847fc0061db738c6cef1cfa12cb25321
Brion Mario <brion.2015283@iit.ac.lk>
2019-06-20 00:17:57 +0530

    chore(model): add payments entity

---
+++ src/main/java/lk/piumalkulasekara/spring/boot/assignment/model/PaymentsEntity.java
@@ -0,0 +1,33 @@
+package lk.piumalkulasekara.spring.boot.assignment.model;
+
+import javax.persistence.Entity;
+
+/**
+ * Copyright (c) 2019. Aparecium Labs.  http://www.apareciumlabs.com
+ *
+ * @author brionsilva
+ * @version 1.0
+ * @since 20/06/2019
+ */
+@Entity
+public class PaymentsEntity {
+
+    private Double amount;
+    private String type;
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
