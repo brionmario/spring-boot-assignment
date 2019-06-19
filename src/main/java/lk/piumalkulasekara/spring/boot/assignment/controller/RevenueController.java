aa54e4bc932327a40046b94f88abb3b6627a28b7
Brion Mario <brion.2015283@iit.ac.lk>
2019-06-20 00:14:19 +0530

    chore(model): add account entity

---
+++ src/main/java/lk/piumalkulasekara/spring/boot/assignment/controller/RevenueController.java
@@ -0,0 +1,18 @@
+package lk.piumalkulasekara.spring.boot.assignment.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Copyright (c) 2019. Aparecium Labs.  http://www.apareciumlabs.com
+ *
+ * @author brionsilva
+ * @version 1.0
+ * @since 19/06/2019
+ */
+@Controller
+public class RevenueController {
+
+}
---
+++ src/main/java/lk/piumalkulasekara/spring/boot/assignment/model/AccountEntity.java
@@ -0,0 +1,38 @@
+package lk.piumalkulasekara.spring.boot.assignment.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Copyright (c) 2019. Aparecium Labs.  http://www.apareciumlabs.com
+ *
+ * @author brionsilva
+ * @version 1.0
+ * @since 20/06/2019
+ */
+@Entity
+public class AccountEntity {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
