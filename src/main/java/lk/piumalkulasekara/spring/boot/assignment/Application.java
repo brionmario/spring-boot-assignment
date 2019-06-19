ea6d185a1f0d386e60c292ee8103bb420ec09a08
Brion Mario <brion.2015283@iit.ac.lk>
2019-06-20 05:04:37 +0530

    chore(core): add metadata

--- src/main/java/lk/piumalkulasekara/spring/boot/assignment/Application.java
+++ src/main/java/lk/piumalkulasekara/spring/boot/assignment/Application.java
@@ -1,5 +1,8 @@
 package lk.piumalkulasekara.spring.boot.assignment;
 
+import lk.piumalkulasekara.spring.boot.assignment.service.MetaDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,10 +14,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 19/06/2019
  */
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    private MetaDataService metaDataService;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        metaDataService.addAccountMetaData();
+        metaDataService.addPaymentMetaData();
+    }
 }
