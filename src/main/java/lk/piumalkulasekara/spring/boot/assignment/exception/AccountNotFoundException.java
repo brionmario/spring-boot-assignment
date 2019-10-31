e3cde81d951d51448ac3ce41d8e07c9154620f37
Melani Silva <melani.2018066@iit.ac.lk>
2019-10-31 23:50:03 +0530

    add exception

--- src/main/java/lk/piumalkulasekara/spring/boot/assignment/exception/AccountNotFoundException.java
+++ src/main/java/lk/piumalkulasekara/spring/boot/assignment/exception/AccountNotFoundException.java
@@ -2,6 +2,6 @@ package lk.piumalkulasekara.spring.boot.assignment.exception;
 
 public class AccountNotFoundException extends RuntimeException {
     public AccountNotFoundException(Integer id) {
-        super("Could not find Account with ID: " + id);
+        super("Could not find Account with ID: " + id + "Please re enter");
     }
 }
